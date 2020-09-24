7d67c153d74509299807f176ca8368dbe2dd9831
Sam Minnee <sam@silverstripe.com>
2020-09-24 19:33:24 +1200

    Fixme - add one more viewabledata test

---
+++ tests/_legacy/ViewableDataTest/UnescapedCaster.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SilverStripe\Core\Tests\ViewableDataTest;
+
+use SilverStripe\Core\Convert;
+use SilverStripe\Dev\TestOnly;
+use SilverStripe\View\ViewableData;
+
+class UnescapedCaster extends ViewableData implements TestOnly
+{
+    protected $value;
+
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    public function forTemplate()
+    {
+        return Convert::raw2xml($this->value);
+    }
+}
