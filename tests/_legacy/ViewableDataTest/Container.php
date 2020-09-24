0804842c2a840501fe27b54cdefaa9d0b9382746
Sam Minnee <sam@silverstripe.com>
2020-09-24 19:04:11 +1200

    Move ViewableData test and support classes
    (Namespaces were also renamed in this patch)

---
+++ tests/_legacy/ViewableDataTest/Container.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace SilverStripe\Core\Tests\ViewableDataTest;
+
+use SilverStripe\Dev\TestOnly;
+use SilverStripe\View\ViewableData;
+
+class Container extends ViewableData implements TestOnly
+{
+
+}
---
+++ tests/_legacy/ViewableDataTest/Failover.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SilverStripe\Core\Tests\ViewableDataTest;
+
+use SilverStripe\Dev\TestOnly;
+use SilverStripe\View\ViewableData;
+
+class Failover extends ViewableData implements TestOnly
+{
+    public function testMethod()
+    {
+    }
+}
